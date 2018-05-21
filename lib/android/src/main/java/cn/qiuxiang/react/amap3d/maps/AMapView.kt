c4b19dfac94ebde728d35ef24ef06c95259ea823
iday <johnny.iday@qq.com>
2018-05-21 12:29:48 +0800

    定位模式调整

--- lib/android/src/main/java/cn/qiuxiang/react/amap3d/maps/AMapView.kt
+++ lib/android/src/main/java/cn/qiuxiang/react/amap3d/maps/AMapView.kt
@@ -19,7 +19,7 @@ class AMapView(context: Context) : TextureMapView(context) {
     private val lines = HashMap<String, AMapPolyline>()
     private val locationStyle by lazy {
         val locationStyle = MyLocationStyle()
-        locationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE)
+        locationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER)
         locationStyle
     }
 
